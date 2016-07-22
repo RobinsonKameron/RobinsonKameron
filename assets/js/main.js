54e13e6c0e57f865af4a3c2a2ebdcf8553bb288a
RobinsonKameron <ka12@umbc.edu>
2016-07-21 17:21:49 -0700

    updating frontend

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+function personalSite(json) {
+  var count = 0;
+  var hellos = [];
+
+  // get all hellos for JSON object
+  console.log("JSON:", json);
+  for (var i = 0; i < json.length; i++) {
+      var str = json[i].Hello
+      if (typeof(str) !== 'undefined') {
+          str = str.charAt(0).toUpperCase().toString() + str.slice(1, str.length)
+          hellos.push(str)
+      } else {
+          str = ""
+      }
+  }
+  console.log("Hellos:", hellos);
+
+  var intervalID = window.setInterval(myCallback, 1500);
+
+  function myCallback() {
+      var sayHello = hellos[count] + "."
+      document.getElementById("language").innerHTML = sayHello
+      document.getElementById("language").className = "fadeIn"
+      count++
+      if (count === hellos.length) {
+          count = 0
+      }
+  }
+}
+
+
+personalSite(json)
