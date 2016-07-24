6eafe1efdb9371c8659e799c9c0ab9cbcecde4ce
RobinsonKameron <ka12@umbc.edu>
2016-07-24 10:54:57 -0700

    Added a bunch of cool stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,35 @@
+(function() {
+    'use strict';
+    personalSite(json)
+    document.getElementById('languageCradel').style.height = "300px"
+}());
+
 function personalSite(json) {
-  var count = 0;
-  var hellos = [];
+    var count = 0;
+    var hellos = [];
 
-  // get all hellos for JSON object
-  console.log("JSON:", json);
-  for (var i = 0; i < json.length; i++) {
-      var str = json[i].Hello
-      if (typeof(str) !== 'undefined') {
-          str = str.charAt(0).toUpperCase().toString() + str.slice(1, str.length)
-          hellos.push(str)
-      } else {
-          str = ""
-      }
-  }
-  console.log("Hellos:", hellos);
+    // get all hellos for JSON object
+    console.log("JSON:", json);
+    for (var i = 0; i < json.length; i++) {
+        var str = json[i].Hello
+        if (typeof(str) !== 'undefined') {
+            str = str.charAt(0).toUpperCase().toString() + str.slice(1, str.length)
+            hellos.push(str)
+        } else {
+            str = ""
+        }
+    }
+    console.log("Hellos:", hellos);
 
-  var intervalID = window.setInterval(myCallback, 1500);
+    var intervalID = window.setInterval(myCallback, 1500);
 
-  function myCallback() {
-      var sayHello = hellos[count] + "."
-      document.getElementById("language").innerHTML = sayHello
-      document.getElementById("language").className = "fadeIn"
-      count++
-      if (count === hellos.length) {
-          count = 0
-      }
-  }
+    function myCallback() {
+        var sayHello = hellos[count] + ","
+        document.getElementById("language").innerHTML = sayHello + " I'm Kam."
+        document.getElementById("language").className = "fadeIn`"
+        count++
+        if (count === hellos.length) {
+            count = 0
+        }
+    }
 }
-
-
-personalSite(json)
