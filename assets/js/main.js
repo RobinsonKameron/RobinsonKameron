eab8927e9a7deae4e20ff2491be565230572e778
RobinsonKameron <ka12@umbc.edu>
2016-07-27 22:20:18 -0700

    💩 update

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,28 +10,29 @@ FOR WHEN I FORGET HOW TO MERGE TO MASTER 😂
 
 (function() {
     'use strict';
-    personalSite(json)
-    document.getElementById('languageCradel').style.height = "300px"
+    init(json);
+    document.getElementById('languageCradel').style.height = "300px";
 }());
 
 
 
-function personalSite(json) {
+function init(json) {
+    'use strict';
     var count = 0;
     var hellos = [];
     for (var i = 0; i < json.length; i++) {
-        var str = json[i].Hello
+        var str = json[i].Hello;
         if (typeof(str) !== 'undefined') {
-            str = str.charAt(0).toUpperCase().toString() + str.slice(1, str.length)
-            hellos.push(str)
+            str = str.charAt(0).toUpperCase().toString() + str.slice(1, str.length);
+            hellos.push(str);
         } else {
-            str = ""
+            str = "";
         }
     }
 
     // OBJECTS
     var language = document.getElementById("language");
-    var newHelloInterval = window.setInterval(pushNewHello, 1500);
+    var newHelloInterval = window.setInterval(pushNewHello, 2000);
 
     // hoverBox.addEventListener("hover", test());
 
@@ -39,13 +40,31 @@ function personalSite(json) {
     // FUNCTIONS
     function pushNewHello() {
         // get all hellos for JSON object
-        var sayHello = hellos[count] + ","
-        language.innerHTML = sayHello + " I'm Kam."
-        language.className = "fadeIn"
-        count++
+        var sayHello = hellos[count] + ",";
+        language.innerHTML = sayHello + " I'm Kam.";
+        language.className = "fadeIn";
+        count++;
         if (count === hellos.length) {
-            count = 0
+            count = 0;
         }
     } // pushNewHello
+
+    var footerInterval = window.setInterval(hideFooter, 1600);
+    var navFooter = document.getElementById('navFooter');
+    var userHasScrolled = false;
+
+    window.onscroll = function(e) {
+        userHasScrolled = true;
+    }
+
+    function hideFooter() {
+        navFooter.style.height = "10px";
+        if (userHasScrolled) {
+            navFooter.style.transition = ".5s ease";
+            navFooter.style.height = "50px";
+            userHasScrolled = false;
+        }
+    }
+
 
 }
