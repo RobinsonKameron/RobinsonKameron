abc5cbf332cee108e7f06898c87728b3ce17c138
RobinsonKameron <ka12@umbc.edu>
2017-04-07 14:26:28 -0400

    Made Projects component more reusable

--- src/Projects/Project.js
+++ src/Projects/Project.js
@@ -1,21 +1,25 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
+import ImagePanel from '../Parts/ImagePanel';
 
 const Project = (props) => {
   return (
-    <div>
-      <div>
-        <h3>About</h3>
-        <p>
-          {props.data.about}
-        </p>
-      </div>
+    <div className='Wrapper'>
+      <ImagePanel img={props.data.img} />
+      <section className='right-panel'>
+        <div>
+          <h3>About</h3>
+          <p>
+            {props.data.about}
+          </p>
+        </div>
 
-      <div>
-        <h3>Tech stack</h3>
-        <ul>
-          {props.data.tech.map((t) => <li>{t}</li>)}
-        </ul>
-      </div>
+        <div>
+          <h3>Tech stack</h3>
+          <ul>
+            {props.data.tech.map((t) => <li>{t}</li>)}
+          </ul>
+        </div>
+      </section>
     </div>
   );
 };
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import Project from './Project';
-import ImagePanel from '../Parts/ImagePanel';
+
 import data from '../../public/data.json';
 
 import tempo from '../../public/projects/tempo/tempo.gif';
+import youmeWorld from '../../public/projects/youme.world/youmeWorld.png';
+import temere from '../../public/projects/temere/temere.png';
 
 // get projects as props [{}, {}, {}] => render UI
 const Projects = (props) => {
   return (
-    <div className='Wrapper'>
-      <ImagePanel img={tempo} alt='tempo' />
-      <section className='right-panel'>
-        {data.projects.map((project) => <Project data={project} />)}
-      </section>
+    <div>
+      {data.projects.map((project) => <Project data={project} />)}
     </div>
   );
 };
